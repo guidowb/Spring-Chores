26ce47daf68f6880974b10f4df26ae2cb5bf4ffe
guidowb <guidowb@gmail.com>
2015-03-16 20:42:07 -0500

    Added profile for local database to run outside of CloudFoundry

--- src/main/java/org/guidowb/chores/config/CloudDataSourceConfig.java
+++ src/main/java/org/guidowb/chores/config/CloudDataSourceConfig.java
@@ -3,9 +3,12 @@ package org.guidowb.chores.config;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
 import javax.sql.DataSource;
 
 @Configuration
+@Profile("cloudfoundry")
 public class CloudDataSourceConfig extends AbstractCloudConfig {
 
     @Bean
